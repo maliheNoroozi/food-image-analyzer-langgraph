a201268b1d1f49ad02d570152d9e0fb9e606d6cf
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2025-12-12 19:38:49 +0100

    Add NutrientsAnalyzer class to nutrients.py for analyzing ingredient nutritional data using ChatGPT.

---
+++ src/services/analysis/nutrients.py
@@ -0,0 +1,26 @@
+from loguru import logger
+
+from services.chat_gpt.gpt import ChatGPT
+from services.analysis.schemas import Ingredient, NutrientsResponse
+from services.prompts import MealScannerPrompts
+
+class NutrientsAnalyzer:
+    def __init__(self):
+        self.chat_gpt = ChatGPT()
+
+    def analyze(self, ingredients: list[Ingredient]):
+        try:
+            ingredients_str = "\n".join([str(ing) for ing in ingredients])
+            user_prompt = MealScannerPrompts.NUTRIENT_USER_PROMPT.format(
+                ingredients_list=ingredients_str
+            )
+            result = self.chat_gpt.generate_parsed_response(
+                model='gpt-4.1',
+                system_prompt=MealScannerPrompts.NUTRIENT_SYSTEM_PROMPT,
+                user_prompt=user_prompt,
+                response_format=NutrientsResponse
+            )
+            return result
+        except Exception as error:
+            logger.error(f"Error analyzing ingredients: {error}")
+            raise error
