8a5732c9a6895c4aef09108f9b1240cb493dd91b
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2025-12-12 14:49:34 +0100

    Add functions to encode images from file paths and URLs in image_processing.py, with error logging using loguru.

--- src/services/image_processing.py
+++ src/services/image_processing.py
@@ -1,5 +1,21 @@
 import base64
+import requests
+from loguro import logger
 
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
+def encode_image(image_path: str):
+    try:
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode("utf-8")
+    except Exception as error:
+        logger.error(f"Error encoding image: {error}")
+        raise error
+
+def encode_image_by_url(image_url: str):
+    try:
+        response = requests.get(image_url)
+        return base64.b64encode(response.content).decode("utf-8")
+    except Exception as error:
+        logger.error(f"Error encoding image by url: {error}")
+        raise error
+       
+
