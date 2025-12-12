4efbd2cad818b31de36424e70ff354ee30c154e1
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2025-12-12 19:38:19 +0100

    Enhance MealScannerPrompts class in prompts.py by adding nutrient analysis prompts for estimating nutritional values based on ingredient portions.

--- src/services/prompts.py
+++ src/services/prompts.py
@@ -1,8 +1,25 @@
 class MealScannerPrompts:
     INGREDIENTS_SYSTEM_PROMPT = """
-    You are a helpful assistant that analyzes food images to extract meal names and ingredients with their portions.
-    """
+You are a helpful assistant that analyzes food images to extract meal names and ingredients with their portions.
+"""
 
     INGREDIENTS_USER_PROMPT = """
-    Analyze the image, meal name, extract any ingredients, and estimate their portions.
-    """
+Analyze the image, meal name, extract any ingredients, and estimate their portions.
+"""
+
+    NUTRIENT_SYSTEM_PROMPT = """
+You are a helpful assistant that analyzes food ingredients and their portions and estimates their nutritional values.
+"""
+
+    NUTRIENT_USER_PROMPT= """
+Analyze the foloowing ingredients and and their portions and estimate the following:
+- Total Calories
+- Total Protein in grams
+- Total Carbohydrates in grams
+- Total Fats in grams
+- Total Fiber in grams
+
+Ingredients:
+{ingredients_list}
+"""
+
