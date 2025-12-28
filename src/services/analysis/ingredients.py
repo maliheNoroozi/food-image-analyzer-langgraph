370d2521679d81f5092b259b93a7ac95b2ce2800
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2025-12-28 12:32:48 +0100

    Implement Redis caching in IngredientsAnalyzer for improved performance and response handling

--- src/services/analysis/ingredients.py
+++ src/services/analysis/ingredients.py
@@ -4,15 +4,20 @@ from services.analysis.schemas import IngredientsResponse
 from services.chat_gpt.gpt import ChatGPT
 from services.image_processing import encode_image_by_url
 from services.prompts import FoodImageAnalyzerPrompts
-
+from services.cache.client import RedisService
 
 class IngredientsAnalyzer:
     def __init__(self):
         self.chat_gpt = ChatGPT()
+        self.redis_server = RedisService()
 
     def analyze(self, image_url: str) -> IngredientsResponse:
         try:
             base64_image = encode_image_by_url(image_url)
+            cache_key = f"ingredients:{image_url}"
+            cached_result = self.redis_server.get(cache_key)
+            if cached_result:
+                return IngredientsResponse.model_validate_json(cached_result)
             result = self.chat_gpt.generate_image_response_by_base64_image(
                 model="gpt-4.1",
                 system_prompt=FoodImageAnalyzerPrompts.INGREDIENTS_SYSTEM_PROMPT,
@@ -20,6 +25,7 @@ class IngredientsAnalyzer:
                 base64_image=base64_image,
                 response_format=IngredientsResponse,
             )
+            self.redis_server.set(cache_key, result.model_dump_json())
             return result
         except Exception as error:
             logger.error(f"Error analyzing ingredients: {error}")
