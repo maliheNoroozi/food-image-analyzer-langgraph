4d45a8d61c58232cc436323eaa121c46e83c2fac
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2025-12-28 12:32:56 +0100

    Enhance NutrientsAnalyzer with Redis caching for ingredient analysis results, improving performance and reducing redundant processing.

--- src/services/analysis/nutrients.py
+++ src/services/analysis/nutrients.py
@@ -3,14 +3,21 @@ from loguru import logger
 from services.analysis.schemas import Ingredient, NutrientsResponse
 from services.chat_gpt.gpt import ChatGPT
 from services.prompts import FoodImageAnalyzerPrompts
+from services.cache.client import RedisService
 
 
 class NutrientsAnalyzer:
     def __init__(self):
         self.chat_gpt = ChatGPT()
+        self.redis_server = RedisService()
 
     def analyze(self, ingredients: list[Ingredient]) -> NutrientsResponse:
         try:
+            ingredients_key = "_".join([f"{i.ingredient_name}:{i.portiont}" for i in ingredients])
+            cache_key = f"nutrients:{ingredients_key}"
+            cached_result = self.redis_server.get(cache_key)
+            if cached_result:
+                return NutrientsResponse.model_validate_json(cached_result)
             ingredients_str = "\n".join([str(ingredient) for ingredient in ingredients])
             user_prompt = FoodImageAnalyzerPrompts.NUTRIENT_USER_PROMPT.format(
                 ingredients_list=ingredients_str
@@ -21,6 +28,7 @@ class NutrientsAnalyzer:
                 user_prompt=user_prompt,
                 response_format=NutrientsResponse,
             )
+            self.redis_server.set(cache_key, result.model_dump_json())
             return result
         except Exception as error:
             logger.error(f"Error analyzing ingredients: {error}")
