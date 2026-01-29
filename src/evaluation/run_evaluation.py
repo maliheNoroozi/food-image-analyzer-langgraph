6d5733c8851d66f89b63383f7f9186c1a6f9be77
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2026-01-29 18:26:45 +0100

    Integrate dotenv for environment variable management and enhance logging in evaluation script

--- src/evaluation/run_evaluation.py
+++ src/evaluation/run_evaluation.py
@@ -1,15 +1,16 @@
-from loguru import logger
 from opik import Opik
+from dotenv import find_dotenv, load_dotenv
+from loguru import logger
 
 from evaluation.eval_dataset import evaluate_dataset
 from evaluation.scoring import aggregate_scores
 
+load_dotenv(find_dotenv())
 client = Opik()
 
 if __name__ == "__main__":
     dataset_name = "Nutrition-5K"
     dataset = client.get_dataset(name=dataset_name)
     scores = evaluate_dataset(dataset=dataset)
-    logger.info(scores)
     aggregated_scores = aggregate_scores(scores=scores)
     logger.info(aggregated_scores)
