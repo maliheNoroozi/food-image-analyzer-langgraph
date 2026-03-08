255f40799a64b9d6805fd2075cc90289002e8b34
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2026-03-08 14:44:00 +0100

    Refactor eval_dataset.py to integrate FoodLLM for ingredient and nutrient analysis, replacing previous analyzers. Update dataset field validation and adjust output handling for improved structure and clarity.

--- src/evaluation/eval_dataset.py
+++ src/evaluation/eval_dataset.py
@@ -1,20 +1,18 @@
-from opik import Dataset
 from pydantic import ValidationError
+from services.llm.food_llm import FoodLLM
 
-from evaluation.llm_judging import judge_ingredients
-from evaluation.schema import (
-    DatasetItem,
-    EvaluationOutput,
-    EvaluationResult,
-    NutrientItems,
-)
+from evaluation.llm_judging import IngredientsJudger
 from evaluation.scoring import score_nutrients
-from services.analysis.ingredients import IngredientsAnalyzer
-from services.analysis.nutrients import NutrientsAnalyzer
+from langsmith import Client
+from langsmith.schemas import Dataset
+from loguru import logger
 
 
-REQUIRED_DATASET_FIELDS = (
+REQUIRED_INPUTS_DATASET_FIELDS = (
     "img_url",
+)
+
+REQUIRED_OUTPUTS_DATASET_FIELDS = (
     "ingredients",
     "carbohydrates",
     "protein",
@@ -22,75 +20,78 @@ REQUIRED_DATASET_FIELDS = (
     "total_calories",
 )
 
+
 def evaluate_dataset_item(
     dataset_item: dict,
-    ingredient_analyzer: IngredientsAnalyzer | None = None,
-    nutrient_analyzer: NutrientsAnalyzer | None = None,
+    food_llm: FoodLLM,
 ) -> dict:
-    for key in REQUIRED_DATASET_FIELDS:
-        if key not in dataset_item:
-            raise KeyError(f"Missing required dataset field: {key}")
+    for key in REQUIRED_INPUTS_DATASET_FIELDS:
+        if key not in dataset_item.inputs:
+            raise KeyError(f"Missing required dataset inputs field: {key}")
 
-    ingredient_analyzer = ingredient_analyzer or IngredientsAnalyzer()
-    ingredients_result = ingredient_analyzer.analyze(image_url=dataset_item['img_url'])
+    for key in REQUIRED_OUTPUTS_DATASET_FIELDS:
+        if key not in dataset_item.outputs:
+            raise KeyError(f"Missing required dataset outputs field: {key}")
 
-    nutrient_analyzer = nutrient_analyzer or NutrientsAnalyzer()
-    nutrient_result = nutrient_analyzer.analyze(
-        ingredients=ingredients_result.ingredients
-    )
+    llm_result = food_llm.invoke(image_url=dataset_item.inputs["img_url"])
 
     llm_output = {
         "ingredients": [
-            ingredient.ingredient_name for ingredient in ingredients_result.ingredients
+            ingredient.ingredient_name for ingredient in llm_result["ingredients_response"].ingredients
         ],
         "nutrients": {
-            "carbohydrates": nutrient_result.total_carbohydrates_g,
-            "protein": nutrient_result.total_protein_g,
-            "fat": nutrient_result.total_fats_g,
-            "total_calories": nutrient_result.total_calories,
+            "carbohydrates": llm_result["nutrients_response"].total_carbohydrates_g,
+            "protein": llm_result["nutrients_response"].total_protein_g,
+            "fat": llm_result["nutrients_response"].total_fats_g,
+            "total_calories": llm_result["nutrients_response"].total_calories,
         },
     }
     expected_output = {
-        "ingredients": dataset_item["ingredients"],
+        "ingredients": dataset_item.outputs["ingredients"],
         "nutrients": {
-            "carbohydrates": dataset_item["carbohydrates"],
-            "protein": dataset_item["protein"],
-            "fat": dataset_item["fat"],
-            "total_calories": dataset_item["total_calories"],
+            "carbohydrates": dataset_item.outputs["carbohydrates"],
+            "protein": dataset_item.outputs["protein"],
+            "fat": dataset_item.outputs["fat"],
+            "total_calories": dataset_item.outputs["total_calories"],
         },
     }
 
     return {"llm_output": llm_output, "expected_output": expected_output}
 
-def evaluate_dataset(dataset: Dataset) -> list[dict]:
+
+def evaluate_dataset(client: Client, dataset: Dataset) -> list[dict]:
     scores = []
 
-    ingredient_analyzer = IngredientsAnalyzer()
-    nutrient_analyzer = NutrientsAnalyzer()
+    food_llm = FoodLLM()
+    ingredients_judger = IngredientsJudger()
 
-    for dataset_item in dataset.get_items():
+    for dataset_item in client.list_examples(dataset_id=dataset.id):
         try:
             evaluation_result = evaluate_dataset_item(
                 dataset_item=dataset_item,
-                ingredient_analyzer=ingredient_analyzer,
-                nutrient_analyzer=nutrient_analyzer,
+                food_llm=food_llm,
             )
-            score_ingredients = judge_ingredients(
+
+            score_ingredients = ingredients_judger.invoke(
                 predicted_ingredients=evaluation_result["llm_output"]["ingredients"],
                 expected_ingredients=evaluation_result["expected_output"][
                     "ingredients"
                 ],
             )
+
             score_nutrient = score_nutrients(
                 predicted_nutrients=evaluation_result["llm_output"]["nutrients"],
                 expected_nutrients=evaluation_result["expected_output"]["nutrients"],
             )
+
             scores.append(
                 {
                     "ingredient_score": score_ingredients,
                     "nutrient_score": score_nutrient,
                 }
             )
-        except (KeyError, ValueError, TypeError, ValidationError):
+        except (KeyError, ValueError, TypeError, ValidationError) as error:
+            logger.error({"error": error})
             continue
+        break
     return scores
